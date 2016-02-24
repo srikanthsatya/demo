12e599115f83628a4e4a2f1014fdb6d7d82851b3
bossmool <bossmool@boss>
2016-02-24 14:59:27 +0530

    hai

---
+++ ramyaa.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+int n,i=1;
+printf("enter the number ");
+scanf("%d",&n);
+while(i<=10)
+{
+printf(                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+i=i+1
